50dba95b93352435fb868240dc67a211cb654838
unknown <unknown@test.com>
2016-05-17 23:46:54 +0900

    emacsの設定を追加

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
+;; カラム番号も表示
+(column-number-mode t)
+
+;; ファイルサイズを表示
+(size-indication-mode t)
+
+;; タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+
+;; 行番号を表示する
+(global-linum-mode t)
+
+;; インデントにタブ文字を使用しない
+(setq-default indent-tabs-mode nil)
+
