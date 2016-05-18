faa2d96f98db45884763580e396198f89ef849f8
unknown <unknown@test.com>
2016-05-19 00:14:44 +0900

    リポジトリの追加とanythingのインストール

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,3 +15,20 @@
 ;; インデントにタブ文字を使用しない
 (setq-default indent-tabs-mode nil)
 
+;; TABの表示幅。初期値は8
+(setq-default tab-width 4)
+
+;; auto-installの設定
+(when (require 'package nil t)
+  ;; MELPAを追加
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  ;; MELPA-stableを追加
+  (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+  ;; Marmaladeを追加
+  (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  ;; Orgを追加
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  ;; 初期化
+  (package-initialize))
+
+
